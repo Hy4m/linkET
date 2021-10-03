b16124d085ca327d05575145e548cbd3174a9307
houyun <houyunhuang@163.com>
2021-10-03 21:09:08 +0800

    color palettes for mantel test plot

---
+++ R/color-pal.R
@@ -0,0 +1,23 @@
+#' @title Colour Palettes
+#' @description Colour palettes for mantel test plot.
+#' @param n integer, should be less than 6.
+#' @return a palette.
+#' @rdname color_pal
+#' @author Hou Yun
+#' @export
+#' @examples
+#' color_pal(4)
+color_pal <- function (n)
+{
+  stopifnot(n <= 6)
+  colors <- c("#D95F02", "#1B9E77", "#7570B3", "#E7298A", "#A6761D",
+              "#CCCCCC99")
+  if (n == 1)
+    return(colors[1])
+  col <- c(colors[1:(n - 1)], colors[6])
+  col
+}
+
+#' @rdname color_pal
+#' @export
+colour_pal <- color_pal
