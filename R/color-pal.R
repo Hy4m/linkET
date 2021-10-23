1e55bcbde7f68d73c0de6d8179a996b1f5a0b6c0
houyun <houyunhuang@163.com>
2021-10-23 14:21:05 +0800

    add alpha params

--- R/color-pal.R
+++ R/color-pal.R
@@ -1,17 +1,18 @@
 #' @title Colour Palettes
 #' @description Colour palettes for mantel test plot.
 #' @param n integer, should be less than 6.
+#' @param alpha alpha level in [0,1].
 #' @return a palette.
 #' @rdname color_pal
 #' @author Hou Yun
 #' @export
 #' @examples
 #' color_pal(4)
-color_pal <- function (n)
+color_pal <- function (n, alpha = 0.2)
 {
   stopifnot(n <= 6)
   colors <- c("#D95F02", "#1B9E77", "#7570B3", "#E7298A", "#A6761D",
-              "#CCCCCC99")
+              scales::alpha("#CCCCCC", alpha = alpha))
   if (n == 1)
     return(colors[1])
   col <- c(colors[1:(n - 1)], colors[6])
