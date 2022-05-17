f41fff3146e96d96c5e4a206c3a38d228da9c581
houyun <houyunhuang@163.com>
2022-05-17 21:27:25 +0800

    delete random message

--- R/zzz.R
+++ R/zzz.R
@@ -4,14 +4,5 @@
                   "with `linkET`, please detach or uninstall the `ggcor` package first.")
     packageStartupMessage(msg)
   }
-
-  if (!("ggcor" %in% loadedNamespaces()) &&
-      ("ggcor" %in% row.names(utils::installed.packages()))) {
-    if (stats::runif(1) > 0.5) {
-      msg <- paste0("`linkET` has almost all of `ggcor`'s features and includes\n",
-                    "many new features, so switching to `linkET` is recommended.")
-      packageStartupMessage(msg)
-    }
-  }
 }
 
