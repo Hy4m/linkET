2d5494ebad285b6592bd6539476926a9bad60067
houyun <houyunhuang@163.com>
2022-06-08 20:55:28 +0800

    register as.igraph and as_tbl_graph when load

--- R/zzz.R
+++ R/zzz.R
@@ -8,4 +8,28 @@
 
 .onLoad <- function(...) {
   register_pairs_plot(reset = TRUE)
+  for(g in c("cor_md_tbl", "correlate", "mantel_tbl", "pro_tbl",
+             "easycorrelation", "rcorr", "corr.test")) {
+    register_s3_method("igraph", "as.igraph", g)
+    register_s3_method("tidygraph", "as_tbl_graph", g)
+  }
+}
+
+register_s3_method <- function (pkg, generic, class, fun = NULL)
+{
+  stopifnot(is.character(pkg), length(pkg) == 1)
+  stopifnot(is.character(generic), length(generic) == 1)
+  stopifnot(is.character(class), length(class) == 1)
+  if (is.null(fun)) {
+    fun <- get(paste0(generic, ".", class), envir = parent.frame())
+  }
+  else {
+    stopifnot(is.function(fun))
+  }
+  if (pkg %in% loadedNamespaces()) {
+    registerS3method(generic, class, fun, envir = asNamespace(pkg))
+  }
+  setHook(packageEvent(pkg, "onLoad"), function(...) {
+    registerS3method(generic, class, fun, envir = asNamespace(pkg))
+  })
 }
