165f14c776ab7cecf05ab0b6eab60dc9af9ca06c
houyun <houyunhuang@163.com>
2022-07-07 23:09:27 +0800

    improve as.igraph

--- R/network.R
+++ R/network.R
@@ -1,33 +1,65 @@
 #' @title Coerce to a Graph
 #' @description Functions to coerce a object to graph if possible.
 #' @param x any \code{R} object.
+#' @param use_adjacency logical, if TRUE will build graph from adjacency matrix.
+#' @param diag logical, if TRUE will keep the diagonal of adjacency matrix data.
 #' @param simplify if TRUE, Simple graphs are graphs which do not contain loop
 #' and multiple edges.
 #' @param directed whether or not to create a directed graph.
 #' @param ... other parameters.
 #' @return  a graph object.
-#' @importFrom igraph graph_from_data_frame
 #' @importFrom igraph as.igraph
 #' @importFrom tidygraph as_tbl_graph
 #' @author Hou Yun
 #' @rdname network
 as.igraph.cor_md_tbl <- function(x,
                                  ...,
+                                 use_adjacency = FALSE,
+                                 diag = FALSE,
                                  simplify = TRUE,
                                  directed = FALSE) {
   rnm <- row_names(x)
   cnm <- col_names(x)
-  x <- dplyr::filter(x, ...)
-
-  if(isTRUE(simplify)) {
-    if(identical(rnm, cnm) && isFALSE(directed)) {
-      x <- extract_upper(x, FALSE)
-    }
-    nodes <- unique(c(x$.rownames, x$.colnames))
+  params <- list(...)
+  if (is.null(params)) {
+    gparams <- list()
   } else {
-    nodes <- unique(c(rnm, cnm))
+    gparams <- params[names(params) != ""]
+    params <- unname(params[names(params) == ""])
   }
-  graph_from_data_frame(x, directed = directed, vertices = nodes)
+  x <- do.call(dplyr::filter, c(list(.data = x), params))
+
+  if (isFALSE(use_adjacency)) {
+    if(isTRUE(simplify)) {
+      if(identical(rnm, cnm) && isFALSE(directed)) {
+        x <- extract_upper(x, diag)
+      }
+      nodes <- unique(c(x$.rownames, x$.colnames))
+    } else {
+      nodes <- unique(c(rnm, cnm))
+    }
+    igraph::graph_from_data_frame(x, directed = directed, vertices = nodes)
+  } else {
+    if(isTRUE(simplify)) {
+      if(identical(rnm, cnm) && isFALSE(directed)) {
+        x <- extract_upper(x, diag)
+      }
+    }
+
+    if (isTRUE(directed)) {
+      mode <- "directed"
+    } else {
+      if (isTRUE(simplify)) {
+        mode <- "max"
+      } else {
+        mode <- "undirected"
+      }
+    }
+
+    do.call(igraph::graph_from_adjacency_matrix,
+            c(list(adjmatrix = adjacency_matrix(x),
+                   mode = mode, diag = diag, weighted = TRUE), gparams))
+  }
 }
 
 #' @rdname network
@@ -104,7 +136,7 @@ adjacency_matrix.cor_md_tbl <- function(x, ...) {
 
 #' @method adjacency_matrix default
 adjacency_matrix.default <- function(x, ...) {
-  adjacency_matrix(as_md_tbl(x), ...)
+  adjacency_matrix(as_md_tbl(x, ...))
 }
 
 #' @noRd
