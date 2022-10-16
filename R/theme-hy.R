c4df00883341a1a7efe65630cc977b2957e3d05c
houyun <houyunhuang@163.com>
2022-10-16 23:29:37 +0800

    remove richtext

--- R/theme-hy.R
+++ R/theme-hy.R
@@ -1,8 +1,6 @@
 #' Create the default hyplot theme
 #' @title hyplot theme
 #' @param ... extra params passing to \code{\link[ggplot2]{theme}}.
-#' @param use_md logical. if TRUE, will use \code{ggtext::element_markdown()} to
-#' draw the axis labels.
 #' @return The theme.
 #' @importFrom ggplot2 theme
 #' @importFrom ggplot2 element_text
@@ -10,28 +8,14 @@
 #' @rdname theme_hy
 #' @author Hou Yun
 #' @export
-theme_hy <- function(..., use_md = FALSE)
+theme_hy <- function(...)
 {
-  if(isFALSE(use_md)) {
-    theme(
-      axis.text = element_text(size = 10.5, colour = "black"),
-      axis.title = element_blank(),
-      axis.line = element_blank(),
-      axis.text.x.top = element_text(angle = 90, hjust = 0, vjust = 0.5),
-      axis.text.x.bottom = element_text(angle = 90, hjust = 1, vjust = 0.5),
-      ...
-    )
-  } else {
-    element_markdown <- get_function("ggtext", "element_markdown")
-    theme(
-      axis.text = element_markdown(size = 10.5, colour = "black"),
-      axis.title = element_blank(),
-      axis.line = element_blank(),
-      axis.text.y.left = element_markdown(),
-      axis.text.y.right = element_markdown(),
-      axis.text.x.top = element_markdown(angle = 90, hjust = 0, vjust = 0.5),
-      axis.text.x.bottom = element_markdown(angle = 90, hjust = 1, vjust = 0.5),
-      ...
-    )
-  }
+  theme(
+    axis.text = element_text(size = 10.5, colour = "black"),
+    axis.title = element_blank(),
+    axis.line = element_blank(),
+    axis.text.x.top = element_text(angle = 90, hjust = 0, vjust = 0.5),
+    axis.text.x.bottom = element_text(angle = 90, hjust = 1, vjust = 0.5),
+    ...
+  )
 }
